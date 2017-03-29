9abc879331261eb8b030dde50abb4df0f3580f06
Antoni Virós Martin <ani300@gmail.com>
2017-03-29 13:19:05 -0400

    Started adding the instruments scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,12 +13,9 @@ class CEOSDBSpider(scrapy.Spider):
     def start_requests(self):
         # Define subclasses
         self.define_subclasses()
-
-        urls = ['http://database.eohandbook.com/database/instrumenttable.aspx']
         yield scrapy.Request(url='http://database.eohandbook.com/database/agencytable.aspx', callback=self.parse_agencies)
         yield scrapy.Request(url='http://database.eohandbook.com/database/missiontable.aspx', callback=self.prepare_missions)
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url='http://database.eohandbook.com/database/instrumenttable.aspx', callback=self.prepare_instruments)
 
     def parse(self, response):
         TITLE_SELECTOR = 'title ::text'
@@ -97,13 +94,60 @@ class CEOSDBSpider(scrapy.Spider):
             self.g.add((mission, CEOSDB_schema.hasApplications, Literal(applications)))
             self.g.add((mission, CEOSDB_schema.hasOrbitDetails, Literal(orbit_details)))
 
-            next_page = response.css('li.next a::attr(href)').extract_first()
-            if next_page is not None:
-                next_page = response.urljoin(next_page)
-                yield scrapy.Request(next_page, callback=self.parse)
+    def prepare_instruments(self, response):
+        sel = scrapy.Selector(response)
+        viewstate = sel.xpath("//input[@id='__VIEWSTATE']/@value").extract().pop()
+        eventvalidation = sel.xpath("//input[@id='__EVENTVALIDATION']/@value").extract().pop()
+        data = {'__EVENTTARGET': '', '__EVENTARGUMENT': '', '__LASTFOCUS': '', '__VIEWSTATE': viewstate,
+                '__VIEWSTATEGENERATOR': 'F2417B6C', '__VIEWSTATEENCRYPTED': '', '__EVENTVALIDATION': eventvalidation,
+                'ddlAgency': 'All', 'ddlMissionStatus': 'All', 'tbMission': '', 'ddlInstrumentStatus': 'All',
+                'ddlLauchYearFilterType': 'All', 'tbInstrument': '', 'ddlInstrumentType': 'All', 'tbApplications': '',
+                'ddlInstrumentTechnology': 'All', 'ddlResolutionFilter': 'All', 'ddlWaveband': 'All',
+                'ddlDataAccess': 'All', 'ddlDisplayResults': 'All', 'btFilter': 'Apply+Filter'}
+        yield scrapy.FormRequest('http://database.eohandbook.com/database/instrumenttable.aspx', formdata = data, callback = self.parse_instruments)
 
     def parse_instruments(self, response):
-        return None
+        TR_SELECTOR = "#gvInstrumentTable > tr"
+        date_parsing_settings = {'RELATIVE_BASE': datetime.datetime(2020, 1, 1)}
+        for row in response.css(TR_SELECTOR)[1:-1]:
+            instrument_name = row.css("td:nth-child(1) b a ::text").extract_first().strip()
+            instrument_id = row.css("td:nth-child(1) b a ::attr(href)").extract_first().strip().split('=', 1)[-1]
+            instrument_fullname = row.css("td:nth-child(1) ::text").extract()
+            if len(instrument_fullname) > 1:
+                instrument_fullname = instrument_fullname[1].strip()
+            else:
+                instrument_fullname = None
+            agency_id = row.css("td:nth-child(2) a ::attr(href)").extract_first()
+            if agency_id is not None:
+                agency_id = agency_id.strip().split('=', 1)[-1]
+            else:
+                agency_id = row.css("td:nth-child(2) ::text").extract_first()
+            # status = row.css("td:nth-child(3) ::text").extract_first().strip()
+            # launch_date = row.css("td:nth-child(4) ::text").extract_first()
+            # if launch_date is not None:
+            #     launch_date = dateparser.parse(launch_date.strip(), settings=date_parsing_settings)
+            # eol_date = row.css("td:nth-child(5) ::text").extract_first()
+            # if eol_date is not None:
+            #     eol_date = dateparser.parse(eol_date.strip(), settings=date_parsing_settings)
+            # applications = row.css("td:nth-child(6) ::text").extract_first().strip()
+            # orbit_details = row.css("td:nth-child(8) ::text").extract_first().strip()  # TODO: Save more detailed orbits!
+            print(instrument_name, instrument_id, instrument_fullname, agency_id)
+            mission = URIRef("http://ceosdb/instrument#" + instrument_id)
+            self.g.add((mission, RDFS.label, Literal(instrument_name)))
+            self.g.add((mission, RDF.type, CEOSDB_schema.instrumentClass))
+            if instrument_fullname is not None:
+                self.g.add((mission, CEOSDB_schema.hasFullName, Literal(instrument_fullname)))
+            if agency_id.isdigit():
+                self.g.add((mission, CEOSDB_schema.builtBy, URIRef("http://ceosdb/agency#" + agency_id)))
+            else:
+                self.g.add((mission, CEOSDB_schema.builtBy, Literal(agency_id)))
+            # self.g.add((mission, CEOSDB_schema.hasStatus, Literal(status)))
+            # if launch_date is not None:
+            #     self.g.add((mission, CEOSDB_schema.hasLaunchDate, Literal(launch_date)))
+            # if eol_date is not None:
+            #     self.g.add((mission, CEOSDB_schema.hasEOLDate, Literal(eol_date)))
+            # self.g.add((mission, CEOSDB_schema.hasApplications, Literal(applications)))
+            # self.g.add((mission, CEOSDB_schema.hasOrbitDetails, Literal(orbit_details)))
 
     def define_subclasses(self):
         self.g.add((CEOSDB_schema.agencyClass, RDFS.subClassOf, OWL.Thing))
